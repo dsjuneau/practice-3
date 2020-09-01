8a60fea1d1cf7d9cd658ed3b84806e98963133b8
Scott Juneau <dsjuneau@gmail.com>
2020-09-01 07:53:05 -0500

    added answer.js

---
+++ answer.js
@@ -0,0 +1,35 @@
+console.log(log1("first"));
+
+// The function declaration (function statement) defines a function with the specified parameters.
+
+function log1(a) {
+  console.log(a);
+  return a;
+}
+
+// Here are two examples of anonymous function expressions (the name is not used):
+
+const log2 = (a) => {
+  console.log(a);
+  return a;
+};
+
+const log3 = function (a) {
+  console.log(a);
+  return a;
+};
+
+// It is also possible to provide a name inside the definition in order to create a named function expression:
+// This can be a valuable tool when debugging.
+
+const log4 = function logIt(a) {
+  console.log(a);
+  return a;
+};
+
+console.log(log2("second"));
+console.log(log3("third"));
+console.log(log4("fourth"));
+
+// Function declarations are hoisted.  This means you call call the function before it is defined.
+// Function expressions are not hoisted.  This means the variable is hoisted, but the function itself is not hoisted.
